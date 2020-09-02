3c146397c4c2734624484f73acf29af8625df31f
Lut99 <mullertim99@gmail.com>
2020-09-02 22:05:05 +0200

    Working on identifying patterns in the passwords and mutating the dict words to obtain new possible words. Currently, combining two words takes much too much memory, so we might want to do that on the spot (together with letter permutation for more than one, perhaps)

--- scripts/dict_generator.py
+++ scripts/dict_generator.py
@@ -4,7 +4,7 @@
 # Created:
 #   02/09/2020, 20:50:33
 # Last edited:
-#   02/09/2020, 21:17:03
+#   02/09/2020, 22:02:43
 # Auto updated?
 #   Yes
 #
@@ -17,6 +17,50 @@
 import argparse
 import os
 import re
+
+
+letter_map = {
+    "a" : ["/-\\", "/\\"],
+    #"b" : "a",
+    "c" : "<",
+    "d" : "|)",
+    "e" : "3",
+    #"f" : "a",
+    "g" : "&",
+    #"h" : "a",
+    "i" : "!",
+    "j" : "]",
+    "k" : "|(",
+    #"l" : "a",
+    "m" : "/V\\",
+    "n" : "[\\]",
+    "o" : ["0", "()", "°"],
+    "p" : "|>",
+    #"q" : "a",
+    #"r" : "a",
+    #"s" : "a",
+    "t" : "7",
+    "u" : "|_|",
+    #"v" : "a",
+    "w" : "\\|/",
+    #"x" : "a",
+    "y" : "`/"#,
+    #"z" : "a",
+}
+month_map = {
+    1: 31,
+    2: 28,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31
+}
 
 
 pattern = re.compile("[\W_]+")
@@ -56,14 +100,60 @@ def main(dicts, output):
                         words.add(word)
 
         i += 1
-    print(f"Read {len(words)} passwords\n")
+    print(f"Done, read {len(words)} passwords\n")
 
     print("Applying mutations...")
-    print("Done\n")
+    complete_words = set()
+    j = 1
+    for word in words:
+        complete_words.add(word)
+
+        # Mutation 1: Replace each letter by a capital (and once all caps)
+        for i in range(len(word)):
+            complete_words.add(word[:i] + word[i].upper() + word[i + 1:])
+        complete_words.add(word.upper())
+        
+        # Mutation 2: Replace certain letters with words
+        for i in range(len(word)):
+            letter = word[i]
+            if letter in letter_map:
+                letter = letter_map[letter]
+            if type(letter) == list:
+                for l in letter:
+                    complete_words.add(word[:i] + l + word[i + 1:])
+            else:
+                complete_words.add(word[:i] + letter + word[i + 1:])
+        
+        # Mutation 3: Add all years between 1970-2000 as years and their '97 form
+        complete_words.add(word + "2000")
+        complete_words.add(word + "00")
+        for year in range(70, 100):
+            complete_words.add(word + f"19{year}")
+            complete_words.add(word + f"{year}")
+
+        print(f"   (Word {j}/{len(words)})", end="\r")
+        j += 1
+    
+    # Mutation 5: Finally, generate a set of all birthdays between 1970 and 2000
+    for year in range(1970, 2001):
+        for month in range(1, 13):
+            for day in range(1, (29 if (month == 2 and year % 4 and year != 2000) else month_map[month]) + 1):
+                if (month == 2 and year % 4 and year != 2000):
+                    day += 1
+                complete_words.add(f"{day:02}{month:02}{year}")
+                complete_words.add(f"{day:02}{month:02}{str(year)[-2:]}")
+                complete_words.add(f"{month:02}{day:02}{year}")
+                complete_words.add(f"{month:02}{day:02}{str(year)[-2:]}")
+                complete_words.add(f"{day}{month}{year}")
+                complete_words.add(f"{day}{month}{str(year)[-2:]}")
+                complete_words.add(f"{month}{day}{year}")
+                complete_words.add(f"{month}{day}{str(year)[-2:]}")
+
+    print(f"\nDone, mutated to {len(complete_words)} passwords\n")
 
     print("Writing to file...")
     with open(output, "w") as f:
-        for w in words:
+        for w in complete_words:
             f.write(w + "\n")
     print("Done\n")
 
